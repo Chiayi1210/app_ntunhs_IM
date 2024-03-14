ed10d42a95394b5dcc17892d4688049f06e2aa68
Chiayi1210 <113504433+Chiayi1210@users.noreply.github.com>
2024-03-14 16:35:37 +0800

    3-140435

---
+++ W3/app/src/main/java/com/example/a314/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a314
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+
+class MainActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
