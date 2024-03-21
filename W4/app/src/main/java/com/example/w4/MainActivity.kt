a7e5bff588c3717b6d1a99e38a1d3d7ca17bd129
Chiayi1210 <113504433+Chiayi1210@users.noreply.github.com>
2024-03-21 16:06:30 +0800

    3/21

--- W4/app/src/main/java/com/example/w4/MainActivity.kt
+++ W4/app/src/main/java/com/example/w4/MainActivity.kt
@@ -8,16 +8,27 @@ import org.w3c.dom.Text
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    val txtCom = findViewById<TextView>(R.id.txtCom)
-    val txtResult = findViewById<TextView>(R.id.txtResult)
-    val btnScissors = findViewById<Button>(R.id.btnSicssors)
-    val btnRock = findViewById<Button>(R.id.btnRock)
-    val btnPaper = findViewById<Button>(R.id.btnPaper)
+
+    private lateinit var txtCom: TextView
+    private lateinit var txtResult:TextView
+    private lateinit var btnScissors:Button
+    private lateinit var btnRock:Button
+    private lateinit var btnPaper:Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txtCom = findViewById<TextView>(R.id.txtCom)
+        txtResult = findViewById<TextView>(R.id.txtResult)
+        btnScissors = findViewById<Button>(R.id.btnSicssors)
+        btnRock = findViewById<Button>(R.id.btnRock)
+        btnPaper = findViewById<Button>(R.id.btnPaper)
+
     }
+
+//石頭
     btnRock.setOnClickstener{
         val iComplay = Random().nextInt( 3)+1
         when(iComplay){
@@ -35,4 +46,41 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+    //剪刀
+    btnScissors.setOnClickstener{
+        val iComplay = Random().nextInt( 3)+1
+        when(iComplay){
+            1-> {
+                txtCom.setText(R.string.scissors)
+                txtResult.setText(getString(R.string.draw))
+            }
+            2-> {
+                txtCom.setText(R.string.rock)
+                txtResult.setText(getString(R.string.lose))
+            }
+            3-> {
+                txtCom.setText(R.string.paper)
+                txtResult.setText(getString(R.string.win))
+            }
+        }
+    }
+    //布
+    btnPaper.setOnClickstener{
+        val iComplay = Random().nextInt( 3)+1
+        when(iComplay){
+            1-> {
+                txtCom.setText(R.string.scissors)
+                txtResult.setText(getString(R.string.lose))
+            }
+            2-> {
+                txtCom.setText(R.string.rock)
+                txtResult.setText(getString(R.string.win))
+            }
+            3-> {
+                txtCom.setText(R.string.paper)
+                txtResult.setText(getString(R.string.draw))
+            }
+        }
+    }
+
 }
