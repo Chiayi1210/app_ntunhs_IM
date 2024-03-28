fa7e27400e5a280653a27a1652e394ae2c2c7608
Chiayi1210 <113504433+Chiayi1210@users.noreply.github.com>
2024-03-28 16:01:13 +0800

    3/28

--- W5/hw/app/src/main/java/com/example/hw5/MainActivity.kt
+++ W5/hw/app/src/main/java/com/example/hw5/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.hw5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val guess = findViewById<Button>(R.id.guess)
+        val play = findViewById<Button>(R.id.play)
+
+        guess.setOnClickListener{
+            val guessIntent = Intent(this,guess_number::class .java)
+            startActivity(guessIntent)
+        }
+
+        play.setOnClickListener{
+            val playIntent = Intent(this,PSS::class.java)
+            startActivity(playIntent)
+        }
+
     }
 }
